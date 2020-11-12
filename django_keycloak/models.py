dd00c99afde96c6a6e9e29c6401e0a4860cd312e
pcoelho <pcoelho@ubiwhere.com>
2020-11-12 11:50:57 +0000

    update model and firs migration

--- django_keycloak/models.py
+++ django_keycloak/models.py
@@ -1,27 +1,23 @@
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
-from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
-#from .managers import CustomUserManager
+from .managers import KeycloakUserManager
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_('email address'), unique=True)
-    # The primary will be the username because it will possible to register
-    # Django admin users. For remote users the username will be the external ID
-    username = models.CharField(_('username'), unique=True, max_length=20,
-                                db_index=True, primary_key=True)
+    username = models.CharField(_("username"), unique=True, max_length=20, primary_key=True)
+    keycloak_id = models.UUIDField(_("keycloak_id"))
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(default=timezone.now)
+    date_joined = models.DateTimeField(auto_now_add=True, blank=True)
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = "username"
+    # REQUIRED_FIELDS = ("keycloak_id", )
 
-    #objects = CustomUserManager()
+    objects = KeycloakUserManager()
 
     def __str__(self):
         return self.username
