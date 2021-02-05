64b466b7b22ff6edbb65fba3cca552146f245b68
pcoelho <pcoelho@ubiwhere.com>
2021-02-05 16:29:52 +0000

    update models

--- django_keycloak/models.py
+++ django_keycloak/models.py
@@ -7,28 +7,6 @@ from .managers import KeycloakUserManager
 
 
 class KeycloakUser(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(
-        _("username"), unique=True, max_length=20, primary_key=True
-    )
-    keycloak_id = models.UUIDField(_("keycloak_id"))
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(auto_now_add=True, blank=True)
-
-    USERNAME_FIELD = "username"
-    # REQUIRED_FIELDS = ("keycloak_id", )
-
-    objects = KeycloakUserManager()
-
-    def __str__(self):
-        return self.username
-
-    class Meta(AbstractBaseUser.Meta):
-        swappable = 'AUTH_USER_MODEL'
-
-
-class AbstractKeycloakUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(_("username"), unique=True, max_length=20)
     keycloak_id = models.UUIDField(_("keycloak_id"), unique=True, primary_key=True)
     is_staff = models.BooleanField(default=False)
@@ -45,11 +23,16 @@ class AbstractKeycloakUser(AbstractBaseUser, PermissionsMixin):
         self._confirm_cache()
         return self._cached_user_info.get("email")
 
-    class Meta:
-        abstract = True
+    class Meta(AbstractBaseUser.Meta):
         swappable = 'AUTH_USER_MODEL'
 
     def _confirm_cache(self):
         if not hasattr(self, "_cached_user_info"):
             keycloak = Connect()
             self._cached_user_info = keycloak.get_user_info_by_id(self.keycloak_id)
+
+
+class AbstractKeycloakUser(KeycloakUser):
+    class Meta:
+        abstract = True
+        swappable = 'AUTH_USER_MODEL'
