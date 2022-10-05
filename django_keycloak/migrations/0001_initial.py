6a2e3ac10253daf1b066515cfb85c126b7a1ca0d
Diogo Silva <49190578+diogosilva30@users.noreply.github.com>
2022-10-05 16:09:46 +0100

    Always use Latest Auth migration as dependencie

--- django_keycloak/migrations/0001_initial.py
+++ django_keycloak/migrations/0001_initial.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ("auth", "0012_alter_user_first_name_max_length"),
+        ("auth", "__latest__"),
     ]
 
     operations = [
