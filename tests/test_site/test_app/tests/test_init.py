aab68390372c0fe57d75b248bf53c75ecb1f8f7c
Diogo Silva <diogosilv30@gmail.com>
2022-10-26 21:14:24 +0100

    Fix Tests

--- tests/test_site/test_app/tests/test_init.py
+++ tests/test_site/test_app/tests/test_init.py
@@ -1,7 +1,8 @@
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django_keycloak.mixins import KeycloakTestMixin
+from django_keycloak.connector import KeycloakAdminConnector
+from django_keycloak import Token
 
 
 class TestInit(KeycloakTestMixin, TestCase):
@@ -11,18 +12,13 @@ class TestInit(KeycloakTestMixin, TestCase):
     def tearDown(self):
         self.keycloak_cleanup()
 
-    def test_pass(self):
-        self.assertTrue(True)
-
     def test_model(self):
-        user_a = get_user_model().objects.create_keycloak_user(
+        user_a = self.create_user_on_keycloak(
             username="ownerA",
             email="user@example.com",
             password="PWowNerA0!",
             first_name="Owner",
             last_name="AAAA",
         )
-        self.keycloak.update_user(user_a.id, emailVerified=True)
-        valid_token = self.keycloak.get_token_from_credentials(
-            username="ownerA", password="PWowNerA0!"
-        )
+        KeycloakAdminConnector.update_user(user_a.id, {"emailVerified": True})
+        valid_token = Token.from_credentials(username="ownerA", password="PWowNerA0!")
